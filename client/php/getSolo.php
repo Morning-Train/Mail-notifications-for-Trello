5a60c822f5b05f5c4ea3a142b6ef6fc359db529c
Rubatharisan Thirumathyam <rubatharisan@gmail.com>
2015-03-10 13:12:20 +0100

    Made getSolo (for solo rows)

---
+++ client/php/getSolo.php
@@ -0,0 +1,16 @@
+<?php
+
+$db = new PDO('mysql:host=localhost;dbname=trellotrain;charset=utf8', 'root', 'root');
+
+if(isset($_GET['id'])){
+	$id = $_GET['id'];
+
+	$stmt = $db->prepare("SELECT * FROM notifiers WHERE id=:id");
+	$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+	$stmt->execute();
+	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	var_dump($rows);
+}
+
+?>
