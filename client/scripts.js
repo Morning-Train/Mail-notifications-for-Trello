bf9f13c5324e3dcdd7b43671bcc031e2f6a24151
jesperkm <jesperkmartinussen@gmail.com>
2015-03-13 13:04:14 +0100

    adding error msg on new record for webhooks

--- client/scripts.js
+++ client/scripts.js
@@ -126,7 +126,7 @@ $(document).ready(function() {
     //
     //
 
-    // Submit new mail notifier
+    // Add new mail notifier
     $('#new-custom-frm').submit(function(e) {
         e.preventDefault();
         //console.log($('#new-custom-frm').serialize());
@@ -325,7 +325,7 @@ $(document).ready(function() {
 
     **/
 
-    /* Webhooks API POST */
+    // Add new record
     $('#web-submit').click(function(e){
         e.preventDefault();
         $.ajax({
@@ -334,8 +334,10 @@ $(document).ready(function() {
             data: $('#web-custom-frm').serialize()
         }).
         success(function(res) {
-            $('#web-success').fadeIn(400).delay(800).fadeOut(800);
-            $('#web-answer').html(res);
+            // Success Feedback
+            $('#submit-succes').empty();
+            $('#submit-success').append('<h3>Created new record!</h3>');
+            $('#submit-success').fadeIn(400).delay(800).fadeOut(800);
 
             // Reset form on submit
             $('#web-custom-frm')[0].reset();
@@ -345,11 +347,15 @@ $(document).ready(function() {
 
         }).
         fail(function(err) {
-            $('#web-error').fadeIn(400).delay(800).fadeOut(800);
+            // Error feedback
+            $('#submit-error').empty();
+            $('#submit-error').append('<h3>Error in creating a new record!</h3>');
+            $('#submit-error').fadeIn(400).delay(800).fadeOut(800);
             $('#web-answer').html(err.responseText);
         });
     })
 
+    // Get all records
     var getAllWebHooks = function(){
         $.get( "mongies/webhooks/all", function( data ) {
                 //$( ".current_webhooks" ).remove();
