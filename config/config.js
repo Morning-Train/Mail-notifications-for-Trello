91b3979899fa081508b0f4ac552dfeb7f13b0b43
Rubatharisan Thirumathyam <rubatharisan@gmail.com>
2015-03-23 14:25:26 +0100

    Hmm

--- config/config.js
+++ config/config.js
@@ -9,19 +9,19 @@ var config = {};
 config.daysBetweenNotifiers = 7; // Default is 7
 
 /* Trello API Access */
-config.trelloApplicationKey = "ef463438274bb639009bX76098f83b026" // Read https://trello.com/docs/gettingstarted/index.html#getting-an-application-key
-config.trelloUserToken = "d0deb23a479200f4274823ca7e9432fcb00306278c4fb1b59bb2d4ad9bbce836" // Read https://trello.com/docs/gettingstarted/index.html#getting-a-token-from-a-user
+config.trelloApplicationKey = "<<INSERT YOUR APPLICATION KEY>>" // Read https://trello.com/docs/gettingstarted/index.html#getting-an-application-key
+config.trelloUserToken = "<<INSERT YOUR TRELLO USER TOKEN>>" // Read https://trello.com/docs/gettingstarted/index.html#getting-a-token-from-a-user
 
 /* Email settings */
-config.myEmail = "mail@morningtrain.dk";
-config.myName = "Morning Train";
+config.myEmail = "<<INSERT YOUR EMAIL>>";
+config.myName = "<<INSERT YOUR NAME>>";
 
 /* SMTP Settings (For outgoing mail)*/
 config.settingsForTransporter = {
-  service: "Mandrill",
+  service: "<<INSERT YOUR MAIL SERVICE>>",
   auth: {
-      user: "mail@morningtrain.dk",
-      pass: "neRUPd59dMEp4CxwaVgfeA"
+      user: "<<INSERT YOUR MAIL USER>>",
+      pass: "<<INSERT YOUR MAIL PASS>>"
   }
 }
 
