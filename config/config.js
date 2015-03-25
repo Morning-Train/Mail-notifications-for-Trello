200e7403097b69dab8936616f88a22c453b82c79
Rubatharisan Thirumathyam <rubatharisan@gmail.com>
2015-03-25 12:36:03 +0100

    Changed config

--- config/config.js
+++ config/config.js
@@ -9,23 +9,24 @@ var config = {};
 config.daysBetweenNotifiers = 7; // Default is 7
 
 /* Trello API Access */
-config.trelloApplicationKey = "ef463438274bb639009b76098f83b026" // Read https://trello.com/docs/gettingstarted/index.html#getting-an-application-key
-config.trelloUserToken = "f0349e689cc7a6bcee4d0200367129217c09ef21fb1ebd77b6573e3a0cad735a" // Read https://trello.com/docs/gettingstarted/index.html#getting-a-token-from-a-user
+config.trelloApplicationKey = "<<Insert your application key here>>" // Read https://trello.com/docs/gettingstarted/index.html#getting-an-application-key
+config.trelloUserToken = "<<Insert your user token here>>" // Read https://trello.com/docs/gettingstarted/index.html#getting-a-token-from-a-user
 
 // Secs & Mins & Hours & Day of Month & Months & Day of Week (Read more here: https://github.com/ncb000gt/node-cron)
-config.crontime = '* * 08 * * 1'; // Default is monday morning at 8
+config.crontime = '* * 08 * * 1';
 config.crontimezone = "Europe/Copenhagen";
 
+
 /* Email settings */
-config.myEmail = "mail@morningtrain.dk";
-config.myName = "Morning Train";
+config.myEmail = "<< Your email >>";
+config.myName = "<< Your name >>";
 
 /* SMTP Settings (For outgoing mail)*/
 config.settingsForTransporter = {
-  service: "Gmail",
+  service: "<< Your service SMTP provider >>",
   auth: {
-      user: "fireflexy@gmail.com",
-      pass: "playtime2a"
+      user: "<< SMTP user >>",
+      pass: "<< SMTP pass >>"
   }
 }
 
