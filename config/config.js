f458719decf97bc293fd2a39fa7f759c029d664d
jesperkm <jesperkmartinussen@gmail.com>
2015-03-25 16:01:30 +0100

    fixing style.css to match splitted application. Changed some of the Jquery duplicates into variables

--- config/config.js
+++ config/config.js
@@ -9,8 +9,8 @@ var config = {};
 config.daysBetweenNotifiers = 7; // Default is 7
 
 /* Trello API Access */
-config.trelloApplicationKey = "<<Insert your application key here>>" // Read https://trello.com/docs/gettingstarted/index.html#getting-an-application-key
-config.trelloUserToken = "<<Insert your user token here>>" // Read https://trello.com/docs/gettingstarted/index.html#getting-a-token-from-a-user
+config.trelloApplicationKey = "b7a8480d496fa90c36f9f2d02020e25b" // Read https://trello.com/docs/gettingstarted/index.html#getting-an-application-key
+config.trelloUserToken = "0bf31a4ea7b08e514839f76a0dda3c5bcb6f133a6a85e6954d4bdb9b03eabc88" // Read https://trello.com/docs/gettingstarted/index.html#getting-a-token-from-a-user
 
 // Secs & Mins & Hours & Day of Month & Months & Day of Week (Read more here: https://github.com/ncb000gt/node-cron)
 config.crontime = '* * 08 * * 1';
