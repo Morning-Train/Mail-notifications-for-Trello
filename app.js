d39f5bd04cc810fcec159d40459a0c61ec262540
rfirevtre <peips@Peters-Air.lan>
2016-02-17 11:56:11 +0100

    billableHours+rounding done + minor fixes

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ var NotifierSchema = new mongoose.Schema({
     email: Array,
     board: String,
     togglProject: String,
+    billableHours: Boolean,
+    rounding: Boolean,
     lists: [{
         list: 'string'
     }],
@@ -70,15 +72,14 @@ require('./controller/notifier')(app, null, Notifier);
 
 // Set a prototype of Date called getWeek
 Date.prototype.getWeek = function() {
-  var date = new Date(this.getTime());
-   date.setHours(0, 0, 0, 0);
-  // Thursday in current week decides the year.
-  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-  // January 4 is always in week 1.
-  var week1 = new Date(date.getFullYear(), 0, 4);
-  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
-                        - 3 + (week1.getDay() + 6) % 7) / 7);
+    var date = new Date(this.getTime());
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    // January 4 is always in week 1.
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
 }
 
 // Get today's date
@@ -196,7 +197,7 @@ var runNewCronJob = function(notifierid) {
                     myNotifiers.push(notify);
                     Boards.push(notify.board);
                 }
-                // Handle notify notifyDay is set to automatic (7)
+                // Handle notify if notifyDay is set to automatic (7)
                 else if (notify.notifyDay === 7) {
                     // Handle notify if it doesn't have a last notification date
                     if (notify.lastNotified === undefined) {
@@ -435,7 +436,7 @@ var setupEmailTemplate = function(userArray, res) {
             emailContent += "<td align='center' style='padding-top: 50px; padding-bottom: 5px; padding-left: 5%; padding-right: 5%;'>" + config.myName + " is working at " + getBoardName(user.boardId) + "</td>";
             emailContent += "</tr>";
             emailContent += "<tr>";
-            emailContent += "<td align='center' style='padding-top: 5px; padding-bottom: 5px; padding-left: 5%; padding-right: 5%;'>Here is a overview of what have changed:</td>";
+            emailContent += "<td align='center' style='padding-top: 5px; padding-bottom: 5px; padding-left: 5%; padding-right: 5%;'>Here is an overview of what have changed:</td>";
             emailContent += "</tr>";
             if (user.overallTime != null) {
                 var x = user.overallTime / 1000;
@@ -671,6 +672,13 @@ var getTogglProjectSummary = function(notify, callback) {
                 "authorization": "Basic " + new Buffer(config.togglApplicationKey + ":" + "api_token").toString("base64")
             }
         };
+
+        if (notify.billableHours === true) {
+            options["path"] += "&billable=yes";
+        }
+        if (notify.rounding === true) {
+            options["path"] += "&rounding=on";
+        }
 
         var req = http.request(options, function(res) {
             if (res.statusCode == 404 || res.statusCode == 403) {
