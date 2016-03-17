0cdf3b7b4658fdf1d35d28849da8a5f8e0bdc78f
p-ipsen <pi@morningtrain.dk>
2016-03-17 12:29:45 +0100

    Updated README & database

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mailnotifiersForTrelloX');
+mongoose.connect('mongodb://localhost/mailnotifiersForTrello');
 
 var http = require("https");
 
